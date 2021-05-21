78b038c7485a61ad2d28ffdbaac9911076a9b8be
Saar Kuzi <saarkuzi@vpnpool-10-250-14-75.near.illinois.edu>
2021-05-21 11:20:42 -0500

    exp

--- iclr17_data.py
+++ iclr17_data.py
@@ -11,8 +11,10 @@ with open(base_dir + 'annotation_aggregated.tsv', 'r') as input_file:
 paper_ids = []
 
 for paper in grades:
-    if grades[paper][1] != '-' and grades[paper][2] != '-' and grades[paper][3]:
+    if grades[paper][1] != '-' and grades[paper][2] != '-' and grades[paper][3] != '-':
         paper_ids.append(paper)
+    else:
+        print(paper)
 
 texts = []
 ids = []
@@ -26,18 +28,18 @@ distilled = [element for element in texts if element[0] in paper_ids]
 texts = distilled
 random.shuffle(texts)
 
-test_size = int(len(texts) * 0.2)
+test_size = 30
 
-test_file = open('dim.all.mod.neu.para.1.test.text', 'w+')
-test_id_file = open('dim.all.mod.neu.para.1.test.ids', 'w+')
+test_file = open('dim.all.mod.neu.para.1.test.val.text', 'w+')
+test_id_file = open('dim.all.mod.neu.para.1.test.val.ids', 'w+')
 
 for i in range(test_size):
     test_file.write(texts[i][1] + '\n')
     test_id_file.write(texts[i][0] + '\n')
 
 
-train_file = open('dim.all.mod.neu.para.1.train.val.text', 'w+')
-train_id_file = open('dim.all.mod.neu.para.1.train.val.ids', 'w+')
+train_file = open('dim.all.mod.neu.para.1.train.text', 'w+')
+train_id_file = open('dim.all.mod.neu.para.1.train.ids', 'w+')
 
 for i in range(test_size, len(texts)):
     train_file.write(texts[i][1] + '\n')
