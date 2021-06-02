cb13d0fd441d955b424ff77c91a62752bdaf4522
Saar Kuzi <saarkuzi@vpnpool-10-251-38-91.near.illinois.edu>
2021-06-02 10:08:17 -0500

    exp

--- embeddings/lstm.py
+++ embeddings/lstm.py
@@ -2,6 +2,7 @@ from keras.models import Model
 from keras.layers import Input, Embedding, LSTM, Dot, Activation, BatchNormalization, Dropout, Dense, Bidirectional, Lambda
 import numpy as np
 import sys
+from tensorflow.keras import backend as K
 
 
 class NeuralModel:
@@ -91,6 +92,7 @@ def train_model(data_name, grades_dim, dimension=20, w_dimension=50, epochs=3, b
 
 
 def infer_embeddings(data_name, grades_dim, model_name, data_type):
+    K.clear_session()
     base_dir = '/home/skuzi2/{}_dataset/'.format(data_name)
     data_dir = base_dir + '/embeddings_data/{}.txt'.format(data_type)
     ids_dir = base_dir + '/data_splits/dim.all.mod.neu.para.1.{}.ids'.format(data_type)
@@ -101,9 +103,8 @@ def infer_embeddings(data_name, grades_dim, model_name, data_type):
 
     model = NeuralModel(n_hidden=int(model_args[4]), embedding_dim=int(model_args[6]))
     compiled_model = model.create_model(weights_dir=model_dir)
-    print(compiled_model.summary())
-    intermediate_layer_model = Model(inputs=compiled_model.get_layer('input_2').output,
-                                     outputs=compiled_model.get_layer('lstm_1').get_output_at(0))
+    intermediate_layer_model = Model(inputs=compiled_model.get_layer('input').output,
+                                     outputs=compiled_model.get_layer('lstm').get_output_at(0))
 
     data, _ = load_data(data_dir, ids_dir, grades_dir, grades_dim, infer_flag=True)
     embeddings = intermediate_layer_model.predict(data)
