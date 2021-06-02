e43ab4f7b16b820f4ce6208f1b3ac177f216e859
Saar Kuzi <saarkuzi@vpnpool-10-251-38-91.near.illinois.edu>
2021-06-02 17:06:37 -0500

    exp

--- supervised_models.py
+++ supervised_models.py
@@ -286,7 +286,7 @@ def lstm_baseline():
     data_name = sys.argv[1]
     dimensions = [5, 15, 25]
     w_dims = [50]
-    epochs = [5]
+    epochs = [3]
     batch_sizes = [1]
     vocabs = [1000]
     lengths = [100]
