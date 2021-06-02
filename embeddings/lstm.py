00d1e0e583fd896722edef613f3271759d54b38a
Saar Kuzi <saarkuzi@vpnpool-10-251-38-91.near.illinois.edu>
2021-06-02 10:09:06 -0500

    exp

--- embeddings/lstm.py
+++ embeddings/lstm.py
@@ -102,7 +102,9 @@ def infer_embeddings(data_name, grades_dim, model_name, data_type):
     model_args = model_name.split('.')
 
     model = NeuralModel(n_hidden=int(model_args[4]), embedding_dim=int(model_args[6]))
+
     compiled_model = model.create_model(weights_dir=model_dir)
+    print(compiled_model.summary())
     intermediate_layer_model = Model(inputs=compiled_model.get_layer('input').output,
                                      outputs=compiled_model.get_layer('lstm').get_output_at(0))
 
