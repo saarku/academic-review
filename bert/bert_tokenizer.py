a214c80ca1380cf95ee8ad82cbcbca8a01edb34b
Saar Kuzi <saarkuzi@vpnpool-10-250-18-235.near.illinois.edu>
2021-06-03 10:59:59 -0500

    exp

---
+++ bert/bert_tokenizer.py
@@ -0,0 +1,44 @@
+import tensorflow_hub as hub
+from bert.tokenization import FullTokenizer
+
+
+class BertTokenizer:
+
+    def __init__(self, sess):
+        self.bert_path = "/home/skuzi2/scibert_scivocab_uncased"
+        self.sess = sess
+        self.tokenizer = self.create_tokenizer_from_hub_module()
+
+    def create_tokenizer_from_hub_module(self):
+        bert_module = hub.Module(self.bert_path)
+        tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+        vocab_file, do_lower_case = self.sess.run([tokenization_info["vocab_file"], tokenization_info["do_lower_case"], ])
+        return FullTokenizer(vocab_file=vocab_file, do_lower_case=do_lower_case)
+
+    def convert_single_example(self, text, max_seq_length=100):
+        text_tokens = self.tokenizer.tokenize(text)
+        if len(text_tokens) > max_seq_length - 2:
+            text_tokens = text_tokens[:(max_seq_length - 2)]
+
+        tokens, segment_ids = [], []
+        tokens.append("[CLS]")
+        segment_ids.append(0)
+        for token in text_tokens:
+            tokens.append(token)
+            segment_ids.append(0)
+        tokens.append("[SEP]")
+        segment_ids.append(0)
+
+        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
+        input_mask = [1] * len(input_ids)
+
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+
+        return input_ids, input_mask, segment_ids
---
+++ bert/training.py
@@ -0,0 +1,16 @@
+from bert_tokenizer import BertTokenizer
+import tensorflow as tf
+sess = tf.Session()
+from tensorflow.keras import backend as K
+
+
+data_dir = '/home/skuzi2/education_dataset/data_splits/dim.all.mod.neu.para.1.train.text'
+bert_tokenizer = BertTokenizer(sess)
+with open(data_dir, 'r') as input_file:
+    for i, line in enumerate(input_file):
+        input_ids, input_mask, segment_ids = bert_tokenizer.convert_single_example(line, max_seq_length=100)
+        print('----------------')
+        print(line)
+        print(input_ids)
+        print(input_mask)
+        print(segment_ids)
