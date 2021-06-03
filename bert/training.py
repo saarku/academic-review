5248b9b22c984e9bb7d94336119de2b97f9d0180
Saar Kuzi <saarkuzi@vpnpool-10-251-89-24.near.illinois.edu>
2021-06-03 15:26:35 -0500

    exp

--- bert/training.py
+++ bert/training.py
@@ -1,5 +1,5 @@
 import tensorflow_hub as hub
-from transformers import AutoModel, TFBertForSequenceClassification
+from transformers import AutoModel, BertForSequenceClassification
 
 #model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
 
@@ -9,6 +9,7 @@ from transformers import AutoModel, TFBertForSequenceClassification
 
 #'/home/skuzi2/scibert_scivocab_uncased''allenai/scibert_scivocab_uncased'
 
-model = TFBertForSequenceClassification.from_pretrained('/home/skuzi2/scibert_scivocab_uncased/bert_')
+model = BertForSequenceClassification.from_pretrained('allenai/scibert_scivocab_uncased')
+print(model)
 
 
