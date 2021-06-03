b4730bff16f684c0244e15ed63b7ed06ad49fea4
Saar Kuzi <saarkuzi@vpnpool-10-250-18-235.near.illinois.edu>
2021-06-03 11:17:55 -0500

    exp

--- bert/bert_tokenizer.py
+++ bert/bert_tokenizer.py
@@ -5,18 +5,17 @@ from bert.tokenization import FullTokenizer
 class BertTokenizer:
 
     def __init__(self, sess):
-        self.bert_path = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
-        #self.bert_path  = "/home/skuzi2/scibert_scivocab_uncased"
+        #self.bert_path = "https://tfhub.dev/google/bert_uncased_L-12_H-768_A-12/1"
+        self.bert_path  = "/home/skuzi2/scibert_scivocab_uncased"
         self.sess = sess
         self.tokenizer = self.create_tokenizer_from_hub_module()
 
     def create_tokenizer_from_hub_module(self):
-        bert_module = hub.Module(self.bert_path)
-        tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
-        vocab_file, do_lower_case = self.sess.run([tokenization_info["vocab_file"], tokenization_info["do_lower_case"], ])
-        print(vocab_file)
-        print(do_lower_case)
-        exit(1)
+        #bert_module = hub.Module(self.bert_path)
+        #tokenization_info = bert_module(signature="tokenization_info", as_dict=True)
+        #vocab_file, do_lower_case = self.sess.run([tokenization_info["vocab_file"], tokenization_info["do_lower_case"], ])
+        vocab_file = self.bert_path + '/vocab.txt'
+        do_lower_case = True
         return FullTokenizer(vocab_file=vocab_file, do_lower_case=do_lower_case)
 
     def convert_single_example(self, text, max_seq_length=100):
