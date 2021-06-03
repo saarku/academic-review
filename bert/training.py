42b72f51b54debc04b6a3bae0e51a465b9ae0f60
Saar Kuzi <saarkuzi@vpnpool-10-250-18-235.near.illinois.edu>
2021-06-03 12:09:32 -0500

    exp

--- bert/training.py
+++ bert/training.py
@@ -1,9 +1,16 @@
 from transformers import AutoTokenizer, AutoModel
+import tensorflow as tf
 
 tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
 model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
 
 train_texts = open('/home/skuzi2/education_dataset/data_splits/dim.all.mod.neu.para.1.train.text', 'r').readlines()
+train_labels = [1]*len(train_texts)
 
 train_encodings = tokenizer(train_texts, truncation=True, padding=True)
-print(train_encodings)
+
+
+train_dataset = tf.data.Dataset.from_tensor_slices((
+    dict(train_encodings),
+    train_labels
+))
