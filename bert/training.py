ee4b9c7c0dd1fa54cba394526a3e22d01f312aed
Saar Kuzi <saarkuzi@vpnpool-10-250-18-235.near.illinois.edu>
2021-06-03 12:02:40 -0500

    exp

--- bert/training.py
+++ bert/training.py
@@ -1,16 +1,9 @@
-from bert_tokenizer import BertTokenizer
-#import tensorflow as tf
-#from tensorflow.keras import backend as K
+from transformers import AutoTokenizer, AutoModel
 
+tokenizer = AutoTokenizer.from_pretrained('/home/skuzi2/scibert_scivocab_uncased')
+model = AutoModel.from_pretrained('/home/skuzi2/scibert_scivocab_uncased')
 
-data_dir = '/home/skuzi2/education_dataset/data_splits/dim.all.mod.neu.para.1.train.text'
-bert_tokenizer = BertTokenizer()
-with open(data_dir, 'r') as input_file:
-    for i, line in enumerate(input_file):
+train_texts = open('/home/skuzi2/education_dataset/data_splits/dim.all.mod.neu.para.1.train.text', 'r').readlines()
 
-        input_ids, input_mask, segment_ids = bert_tokenizer.convert_single_example(line, max_seq_length=100)
-        print('----------------')
-        print(line)
-        print(input_ids)
-        print(input_mask)
-        print(segment_ids)
+train_encodings = tokenizer(train_texts, truncation=True, padding=True)
+print(train_encodings)
