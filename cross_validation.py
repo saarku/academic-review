03dcf253dad3d0f6765250df1b1a2000de67326b
Saar Kuzi <saarkuzi@vpnpool-10-251-90-30.near.illinois.edu>
2021-06-09 17:13:19 -0500

    exp

--- cross_validation.py
+++ cross_validation.py
@@ -306,6 +306,31 @@ def run_topics_experiment():
     output_file.write(output_lines)
 
 
+def run_embeddings_experiment():
+    data_name = sys.argv[1]
+    arch = sys.argv[2]
+
+    data_dir = '/home/skuzi2/{}_dataset'.format(data_name)
+    test_dimensions = {'education': [0, 1, 2, 3, 4, 5, 6], 'iclr17': [1, 2, 3, 5, 6]}[data_name]
+    combination_methods = ['feature_comb', 'comb_sum', 'comb_model']
+    algorithms = ['mlp', 'regression', 'ranking']
+    unigrams = [False, True]
+    output_file = open('report_{}_{}.txt'.format(arch, data_name), 'w+')
+    output_lines, header = '', ''
+
+    train_features, test_features, model_name = get_embedding_vectors(data_dir, arch, test_dimensions, 'cv')
+    for combination in combination_methods:
+        for uni in unigrams:
+            for algo in algorithms:
+                output, header = cv_experiment(test_dimensions, data_dir, uni, combination, train_features,
+                                               test_features, algo, model_name, 'cv')
+                output_lines += output
+                print(output)
+
+    output_file.write(header)
+    output_file.write(output_lines)
+
+
 def neural_comb(test_dimensions, data_dir):
 
     output_performance = ''
@@ -381,7 +406,7 @@ def neural_comb(test_dimensions, data_dir):
 
 
 def main():
-    run_topics_experiment()
+    run_embeddings_experiment()
 
     '''
     #[LDA, LSTM, CNN]
