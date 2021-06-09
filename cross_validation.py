f40dc34e17b4002bb60efebfe591f29142790244
Saar Kuzi <saarkuzi@vpnpool-10-251-90-30.near.illinois.edu>
2021-06-09 17:53:59 -0500

    exp

--- cross_validation.py
+++ cross_validation.py
@@ -24,7 +24,7 @@ def learn_model(algorithm, features, labels, model_dir):
         joblib.dump(clf, model_dir)
 
     elif algorithm == 'mlp':
-        clf = MLPRegressor()
+        clf = MLPRegressor(hidden_layer_sizes=50)
         clf.fit(features, labels)
         joblib.dump(clf, model_dir)
 
