c18725702d4069550164dc48e98f7344abc1914c
Saar Kuzi <saarkuzi@vpnpool-10-250-16-95.near.illinois.edu>
2021-06-14 11:57:00 -0500

    exp

--- cross_validation.py
+++ cross_validation.py
@@ -284,6 +284,7 @@ def get_unigram_representations():
     sorted_kendall = sorted(correlations, key=correlations.get, reverse=True)
     output_lines = ''
 
+    print(sorted_kendall[:20])
     for i in sorted_kendall[:100]:
         output_lines += feature_names[i] + ','
     output_lines += '\n'
