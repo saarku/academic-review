bd077db4ba3dc64b202b1c3c1a471332fedfa74f
Saar Kuzi <saarkuzi@vpnpool-10-251-40-222.near.illinois.edu>
2021-06-17 15:04:18 -0500

    exp

--- topic_models.py
+++ topic_models.py
@@ -240,7 +240,7 @@ class TopicModels:
 def main():
 
     dataset = sys.argv[1]
-    topics = [5]
+    topics = [15, 25]
     modes = ['pos', 'neg']
     dimensions_ed = {'0': modes, '1': modes, '2': modes, '3': modes, '4': modes, '5': modes, '6': modes, 'all': ['neu']}
     dimensions_ic = {'1': modes, '2': modes, '3': modes, 'all': ['neu'], '5': modes, '6': modes}
@@ -249,7 +249,7 @@ def main():
     dimensions = dimensions_ed if dataset == 'education' else dimensions_ic
     model_types = ['ovb', 'gibbs']  # ovb or gibbs
 
-    learn_flag = False
+    learn_flag = True
     infer_flag = True
 
     if learn_flag:
