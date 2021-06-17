af509a0399521bb993a6c399a6435ef984ff5f73
Saar Kuzi <saarkuzi@vpnpool-10-251-40-222.near.illinois.edu>
2021-06-17 12:09:05 -0500

    exp

--- iclr_data.py
+++ iclr_data.py
@@ -21,7 +21,7 @@ def download_iclr19(client, outdir='./', get_pdfs=False):
     reviews_by_forum = defaultdict(list)
     for review in reviews:
         reviews_by_forum[review.forum].append(review)
-    print(reviews_by_forum)
+    print(reviews_by_forum['BJAg3e7ZM'])
 
     meta_reviews = openreview.tools.iterget_notes(
         client, invitation='ICLR.cc/20{}/Conference/-/Paper.*/Meta_Review'.format(year))
