2b7c2961bc8c754c19e6bb8e3f606f4b2dd1d0bb
Saar Kuzi <saarkuzi@vpnpool-10-251-90-217.near.illinois.edu>
2021-06-18 15:56:11 -0500

    exp

--- aspect_ranking.py
+++ aspect_ranking.py
@@ -167,6 +167,7 @@ class SearchEngine:
 
     def get_citation_dcg(self, result_list, cutoff):
         citations = []
+        print(self.citations['Citations'])
         for i, paper_id in enumerate(result_list[:cutoff]):
             citations.append(self.citations['Citations'].get(paper_id, 0))
         dcg = self.get_dcg(citations)
