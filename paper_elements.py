d4396091d8cca105cb9eba3b9edbf78c5cb87af9
Saar Kuzi <saarkuzi@vpnpool-10-251-90-217.near.illinois.edu>
2021-06-18 09:46:28 -0500

    exp

--- paper_elements.py
+++ paper_elements.py
@@ -87,23 +87,44 @@ def get_paper_conference(paper_dir):
 
 
 def main():
+
+    get_fields_flag = False
+    get_citation_count_flag = True
+
     input_dir = '/home/skuzi2/acl_antology_all_papers/grobid_output/'
     files = {'conferences': open('conferences', 'w'), 'authors': open('authors.txt', 'w'),
              'affiliations': open('affiliations.txt', 'w')}
 
-    for file_name in os.listdir(input_dir):
-        paper_dir = input_dir + file_name
-        paper_id = file_name.split('.')[0]
-        affiliations = get_paper_affiliation(paper_dir)
-        authors = get_paper_authors(paper_dir)
-        conference = get_paper_conference(paper_dir)
-        files['conferences'].write(paper_id + ',' + conference + '\n')
-        files['conferences'].flush()
-        files['authors'].write(paper_id + ',' + ','.join(authors) + '\n')
-        files['authors'].flush()
-        files['affiliations'].write(paper_id + ',' + ','.join(affiliations) + '\n')
-        files['affiliations'].flush()
+    if get_fields_flag:
+        for file_name in os.listdir(input_dir):
+            paper_dir = input_dir + file_name
+            paper_id = file_name.split('.')[0]
+            affiliations = get_paper_affiliation(paper_dir)
+            authors = get_paper_authors(paper_dir)
+            conference = get_paper_conference(paper_dir)
+            files['conferences'].write(paper_id + ',' + conference + '\n')
+            files['conferences'].flush()
+            files['authors'].write(paper_id + ',' + ','.join(authors) + '\n')
+            files['authors'].flush()
+            files['affiliations'].write(paper_id + ',' + ','.join(affiliations) + '\n')
+            files['affiliations'].flush()
 
-    
+    if get_citation_count_flag:
+        ref_net_dir = '/home/skuzi2/acl_dataset/ref_net.txt'
+        lines = open(ref_net_dir, 'r').readlines()
+        citation_counts = {}
+
+        for line in lines:
+            args = line.rstrip('\n').split(',')
+            if args[0] not in citation_counts:
+                citation_counts[args[0]] = 0
+            if len(args) > 1:
+                for paper_id in args[1:]:
+                    citation_counts[paper_id] = citation_counts.get(paper_id, 0) + 1
+        output_file = open('citation_counts.txt', 'w+')
+        for paper_id in citation_counts:
+            output_file.write(paper_id + ',' + str(citation_counts[paper_id]) + '\n')
+
+
 if __name__ == '__main__':
     main()
