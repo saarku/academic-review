1f63e6a7fb63dbb145c33093cf0464e1e7983df0
Saar Kuzi <saarkuzi@vpnpool-10-250-19-4.near.illinois.edu>
2021-06-21 17:47:54 -0500

    exp

--- aspect_ranking.py
+++ aspect_ranking.py
@@ -334,7 +334,7 @@ def main():
     #get_titles(sys.argv[1])
     #robustness_evaluations('/Users/saarkuzi/Desktop/iclr_eval.txt')
     #filter_queries('/home/skuzi2/iclr_large/scholar_queries.txt')
-    query = ['question answering']
+    query = ['self attention']
 
     data_name = sys.argv[1]
 
