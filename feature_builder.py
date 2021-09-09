09768ae65a07ff79f5fcd90a570c74636f2cef9e
Saar Kuzi <saarkuzi@vpnpool-10-251-104-60.near.illinois.edu>
2021-09-09 16:56:10 -0700

    exp

--- feature_builder.py
+++ feature_builder.py
@@ -78,7 +78,7 @@ class FeatureBuilder:
                 modified_lines.append(line)
 
         if num_samples < len(modified_lines):
-            random.seed(2)
+            random.seed(3)
             zipped_data = list(zip(modified_lines, modified_grades))
             random.shuffle(zipped_data)
             modified_grades, modified_lines = [], []
